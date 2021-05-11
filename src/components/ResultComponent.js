afac2e02d5ea23c6909993159d785d44d2b84abb
Maher  Bouidani <maher.buedani@gmail.com>
2021-05-11 00:35:01 -0400

    add snack bar banner and format the code

--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import "../styles/SearchResultCss.css";
+import MuiAlert from "@material-ui/lab/Alert";
+import Snackbar from "@material-ui/core/Snackbar";
 import { Typography } from "@material-ui/core";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 class ResultComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { nominations: [] };
+    this.state = { nominations: [], openSnackBar: false };
     this.addNomination = this.addNomination.bind(this);
     this.removeNomination = this.removeNomination.bind(this);
+    this.updateSnackBar = this.updateSnackBar.bind(this);
   }
 
   addNomination(e) {
@@ -21,8 +28,6 @@ class ResultComponent extends React.Component {
         nominations: [...prevState.nominations, curr],
       }));
       e.target.disabled = true;
-    } else {
-      alert("You have selected 5 nominiees");
     }
   }
 
@@ -45,9 +50,24 @@ class ResultComponent extends React.Component {
     this.forceUpdate();
   }
 
+  updateSnackBar(status) {
+    this.setState({ openSnackBar: status });
+  }
+
+
   render() {
     return (
       <div className="root">
+        <Snackbar
+          autoHideDuration={4000}
+          open={this.state.openSnackBar}
+          onClose={() => this.updateSnackBar(false)}
+          
+        >
+          <Alert onClose={() => this.updateSnackBar(false)} severity="success">
+            You have successfully selected your 5 nominees
+          </Alert>
+        </Snackbar>
         <Paper className="search-result" elevation={3}>
           <Typography>Results for "{this.props.search}"</Typography>
           <ul>
@@ -59,7 +79,10 @@ class ResultComponent extends React.Component {
 
                     <button
                       className="button"
-                      onClick={this.addNomination}
+                      onClick={(e) => {
+                        this.addNomination(e);
+                        this.updateSnackBar(this.state.nominations.length >= 4);
+                      }}
                       data={item.imdbID}
                       id={item.imdbID}
                     >
