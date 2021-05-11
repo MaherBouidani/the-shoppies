df55c351efe4b8fe7e61498ebaf461b0465cf3db
Maher  Bouidani <maher.buedani@gmail.com>
2021-05-10 23:43:21 -0400

    styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Paper } from "@material-ui/core";
+import { Typography} from "@material-ui/core";
 import "../styles/LandingPage.css";
 import SearchBox from "./SearchBox";
 
@@ -8,17 +8,11 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div>
-        <div className="header">
-          <Typography align="center" color="textPrimary" variant="h2">
-            {" "}
-            The Shoppies
-          </Typography>
-        </div>
-        <div>
+        <div className ="searchComponent">
             <SearchBox />
         </div>
         <div className="footer">
-          <Typography align="center" color="textSecondary" variant="body2">
+          <Typography align="center" color="inherit" variant="body2">
             Maher Bouidani@2021 Shopify Front End Internship Challenge{" "}
           </Typography>
         </div>
